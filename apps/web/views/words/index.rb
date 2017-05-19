e2bfab9b0c748f8ce23d0a0505b5151e5e877132
Kamil Pluta <detfis@gmail.com>
2017-05-19 08:21:06 +0200

    use haml

--- apps/web/views/words/index.rb
+++ apps/web/views/words/index.rb
@@ -1,5 +1,11 @@
 module Web::Views::Words
   class Index
     include Web::View   
+
+    def delete_word_form(word)
+      form_for :word, routes.destroy_word_path(word.id), method: :delete, class: "delete_word" do
+        submit 'Delete'
+      end
+    end
   end
 end
